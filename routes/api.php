3e7638119b02fa154077540fb6727fe66ac3c8a6
caleb <etemesicaleb@gmail.com>
2022-09-14 11:25:28 +0300

    routes/api: Finished new user config

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,48 @@ Route::get('/users', function (Request $request) {
     return response($response_data, 200);
 
 });
+
+Route::post("new_user", function (Request $request) {
+    $request_data = json_decode($request->getContent(),true);
+    $msg = [];
+    $status = 200;
+
+    if ($request_data == null) {
+        $msg = ["status" => 400, "description" => "Unprocessible entry"];
+        $status = 400;
+
+    } else {
+        $rules = [
+            "email" => "required|unique:users",
+            "password" => "required|min:6",
+            "username" => "required|unique:users",
+            "full_name" => "required",
+            "dob" => "required",
+            "gender"=>"required",
+            "telephone" => "required|unique:users"];
+        try {
+            $request->validate($rules);
+            $user = new User;
+
+            $user = User::create([
+                    'username' => $request_data['username'],
+                    'email' => $request_data['email'],
+                    'password' => Hash::make($request_data['password']),
+                    'full_name' => $request_data["full_name"],
+                    'gender' => $request_data["gender"],
+                    'telephone' => $request_data['telephone'],
+                    'dob' => $request_data['dob']
+                ]
+            );
+            $token = $user->createToken($request_data["username"]);
+            $msg= ["status"=>200,"description"=>"okay","data"=>["token"=>$token->plainTextToken]];
+
+        } catch (Exception $e) {
+            $msg = ["status" => 500, "description" => "Internal error", "error" => $e->getMessage(),"data"=>[]];
+            $status = 500;
+
+        }
+    }
+
+    return response($msg, $status);
+});
