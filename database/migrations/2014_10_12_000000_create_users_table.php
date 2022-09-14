3d7165cfad6feda70199f4d7e50fd087d6537a20
caleb <etemesicaleb@gmail.com>
2022-09-14 08:33:18 +0300

    migration/users: Removed name field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
 
             $table->string('email')->unique();
             $table->string("username")->unique();
