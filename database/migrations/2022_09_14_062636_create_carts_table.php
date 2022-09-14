8432c823bc25800092bd6efa2bcc36b43e3fbbcf
caleb <etemesicaleb@gmail.com>
2022-09-14 09:39:02 +0300

    app/models+db/migrations: Added new models

---
+++ database/migrations/2022_09_14_062636_create_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+};
