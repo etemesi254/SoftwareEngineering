6a0807ee18f405ef4e544a07087eb4f5b3207d37
caleb <etemesicaleb@gmail.com>
2022-09-14 11:25:53 +0300

    app/Kernel:Added Api middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             SubstituteBindings::class,
         ],
