2c8ee21ce9b4c8b5f8bf9369bed823d2d8253220
caleb <etemesicaleb@gmail.com>
2022-09-17 00:16:11 +0300

    app/Models/CategoriesDashboard: Add function to fetch menus, the menu sub-category and the menu category.

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Menu extends Model
 {
     use HasFactory;
+
+    public function GetMenuItems(): array
+    {
+        return DB::select("select menus.id,menus.name,menus.unit_price,menus.description,menus.image,sub_categories.subcategory_name,categories.category_name from categories
+	                            inner join sub_categories on  sub_categories.category_id=categories.id
+                                inner join menus on menus.subcategory_id = sub_categories.id; ");
+    }
 }
