5c5199672ea814cbe450789c85c0501757f1f401
caleb <etemesicaleb@gmail.com>
2022-09-17 21:02:48 +0300

    models/Menu: Replace raw sql with Db schema

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -10,10 +10,23 @@ class Menu extends Model
 {
     use HasFactory;
 
-    public function GetMenuItems(): array
+    public function GetMenuItems($limit = PHP_INT_MAX): \Illuminate\Support\Collection
     {
-        return DB::select("select menus.id,menus.name,menus.unit_price,menus.description,menus.image,sub_categories.subcategory_name,categories.category_name from categories
-	                            inner join sub_categories on  sub_categories.category_id=categories.id
-                                inner join menus on menus.subcategory_id = sub_categories.id; ");
+        /*
+         *  Raw SQL
+         * select menus.id,menus.name,menus.unit_price,menus.description,menus.image,sub_categories.subcategory_name,categories.category_name from categories
+         *	inner join sub_categories on  sub_categories.category_id=categories.id
+         *  inner join menus on menus.subcategory_id = sub_categories.id;
+        */
+        return Db::table('menus')
+            ->join('sub_categories', 'sub_categories.id', "=", "menus.subcategory_id")
+            ->join("categories", "categories.id", "=", "sub_categories.category_id")
+            ->select("menus.id", "menus.name", "menus.unit_price", "menus.description", "menus.image", "sub_categories.subcategory_name", "categories.category_name")
+            ->limit($limit)
+            ->get();
+//        return DB::select("select menus.id,menus.name,menus.unit_price,menus.description,menus.image,sub_categories.subcategory_name,categories.category_name from categories
+//	                            inner join sub_categories on  sub_categories.category_id=categories.id
+//                                inner join menus on menus.subcategory_id = sub_categories.id; ");
     }
+
 }
