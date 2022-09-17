bf126da37543cad1412d5c3e3318aa404899e818
caleb <etemesicaleb@gmail.com>
2022-09-17 21:01:49 +0300

    app/Models/Categories: Added ability to fetch categories based on their count for menus

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -4,11 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Categories extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         "category_name"
     ];
+
+    public function GetCategories(): \Illuminate\Support\Collection
+    {
+        //   select sub_categories.subcategory_name,categories.category_name ,COUNT(menus.subcategory_id)  as counts from menus
+        //	 left join sub_categories on sub_categories.id = menus.subcategory_id
+        //   left join categories on sub_categories.category_id = categories.id
+        //   GROUP BY menus.subcategory_id;
+        return DB::table("menus")
+            ->leftJoin("sub_categories", "sub_categories.id", "=", "menus.subcategory_id")
+            ->leftJoin("categories", "categories.id", "=", "sub_categories.category_id")
+            ->select("sub_categories.subcategory_name", "categories.category_name")
+            ->selectRaw("COUNT(menus.subcategory_id)  as counts")
+            ->groupBy("menus.subcategory_id")
+            ->get();
+
+
+    }
+
+    public function GetTotalCategories(): int
+    {
+        return $this::all()->count();
+    }
 }
