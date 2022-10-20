8dc3d8b825f92e41608250a003d856cfe5d6b551
caleb <etemesicaleb@gmail.com>
2022-10-20 16:32:36 +0300

    chore: Fix alignment issues

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -138,7 +138,7 @@
 
                     </a>
 
-                    <a href="http://127.0.0.1/phpmyadmin/index.php?route=/sql&pos=0&db={{$database_name}}&table=menu"
+                    <a href="http://127.0.0.1/phpmyadmin/index.php?route=/sql&pos=0&db={{$database_name}}&table=menus"
                        style="background-color: rgba(230,96,1,0.2)"
                        class="m-5 p-5  rounded-3xl shadow-lg hover:shadow-2xl flex justify-between flex-col w-40 text-center">
 
@@ -156,8 +156,8 @@
 
 
     <div style="border-left: solid 1px rgba(0,0,0,0.1);height: 100%" class="px-6 my-6">
-        <div style="display: flex;justify-content: space-between;margin-bottom: 30px">
-            <h1 style="font-family: 'Outfit',sans-serif;font-weight: bold;font-size: 30px">
+        <div style="display: flex;justify-content: center;margin-bottom: 30px">
+            <h1 style="font-family: 'Outfit',sans-serif;font-weight: bold;font-size: 30px" class="text-center">
                 Hello <span style="color:#FF9F1C"> {{$user->name}}</span>
             </h1>
         </div>
@@ -165,9 +165,9 @@
             <div style="margin: auto;border-radius: 9999px;background-color: #1b171d;width: 100px;height: 100px"></div>
         </div>
 
-        <div style=" display: flex;justify-content: flex-end;flex-direction: column;">
-            <h3>email:{{$user->email}}</h3>
-        </div>
+{{--        <div style=" display: flex;justify-content: flex-end;flex-direction: column;">--}}
+{{--            <h3>email:{{$user->email}}</h3>--}}
+{{--        </div>--}}
 
         <div>
             <h1 style="font-family: 'Outfit',sans-serif;font-weight: bold;font-size: 23px;color:#ff7720">
