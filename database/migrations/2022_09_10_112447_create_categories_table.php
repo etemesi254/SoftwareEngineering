94e7f360f2a08b9526190ee20035c5b576d04ef7
Ryan <ryankinotikathurima@gmail.com>
2022-11-02 11:03:18 +0300

    accomplishment: no need to register categories

--- database/migrations/2022_09_10_112447_create_categories_table.php
+++ database/migrations/2022_09_10_112447_create_categories_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,8 +17,26 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string("category_name");
+            $table->text("description");
+            $table->text("image");
             $table->timestamps();
         });
+
+//          do not delete this
+//        DB::update("UPDATE `categories` SET
+//                        `category_name`='Breakfast',
+//                        `description`='Meals to start your day',
+//                        `image`='public/images/categories/9RQ9tXuVVfzzv4ucmYpFTxyUUlSjStTAHYe3hkkN.jpg' WHERE 1");
+//
+//        DB::update("UPDATE `categories` SET
+//                        `category_name`='Lunch',
+//                        `description`='Middle of the day? Have a meal with us then',
+//                        `image`='public/images/categories/1FYVQCNLnCX8FxHNJZRfJcFJEmzqLQODGrJyWnUw.jpg' WHERE 1");
+//
+//        DB::update("UPDATE `categories` SET
+//                        `category_name`='Dinner',
+//                        `description`='Nighttime already; Have something to eat before you sleep',
+//                        `image`='public/images/categories/8UAiY5l0lTNsxr7HjlAFFX7I34CPdjLCi2NrUekR.jpg' WHERE 1");
     }
 
     /**
