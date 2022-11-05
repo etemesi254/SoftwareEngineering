e56f8965be8163ef87f6fbf84ee4d8df9349c0c0
caleb <etemesicaleb@gmail.com>
2022-11-05 12:56:44 +0300

    feat: Add users dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -187,7 +187,7 @@
                     </a>
 
 
-                    <a href="/admin/add_menu" style="background-color: rgba(230,96,1,0.2)"
+                    <a href="/admin/view_users" style="background-color: rgba(230,96,1,0.2)"
                        class="m-5 p-5  rounded-3xl shadow-lg hover:shadow-2xl flex justify-between flex-col w-40 text-center">
                         <i class="fa-solid fa-cloud-upload fa-2xl my-5"></i>
 
@@ -221,16 +221,13 @@
     <div style="border-left: solid 1px rgba(0,0,0,0.1);height: 100%" class="px-6 my-6">
         <div style="display: flex;justify-content: center;margin-bottom: 30px">
             <h1 style="font-family: 'Outfit',sans-serif;font-weight: bold;font-size: 30px" class="text-center">
-                Hello <span style="color:#FF9F1C"> {{$user->name}}</span>
+                Hello <span style="color:#FF9F1C"> {{$user->fullname}}</span>
             </h1>
         </div>
         <div class="avatar-image">
             <div style="margin: auto;border-radius: 9999px;background-color: #1b171d;width: 100px;height: 100px"></div>
         </div>
 
-        {{--        <div style=" display: flex;justify-content: flex-end;flex-direction: column;">--}}
-        {{--            <h3>email:{{$user->email}}</h3>--}}
-        {{--        </div>--}}
 
         <div>
             <h1 style="font-family: 'Outfit',sans-serif;font-weight: bold;font-size: 23px;color:#ff7720">
